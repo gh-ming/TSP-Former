aeb8b8241aac6cbf66aac1dddc3a698b9bb5d640
ghming <gh_ming00@163.com>
2025-12-15 15:19:44 +0800

    model: update TSP_Former and clean old TransNet

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,6 @@ from .LTAE import LTAE
 from .TempCNN import TempCNN
 from .LSTM import LSTM
 from .STNet import STNet
-from .TSP_TransNet1 import TSP_TransNet
+from .TSP_Former import TSP_Former
 from .weight_init import weight_init
+from .AlphaEmbedding import AlphaEmbedding
